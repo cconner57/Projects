56c4c52d1459dd0c809d322b8e5aec82dd60887b
cconner57 <chris.conner57@gmail.com>
2019-10-14 16:59:05 -0700

    Added Radium

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -41,12 +41,13 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+    };
 
     let persons = null;
 
@@ -58,12 +59,25 @@ class App extends Component {
           })}
         </div> 
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
