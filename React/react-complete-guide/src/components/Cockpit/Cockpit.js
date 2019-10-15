a3e62e773426dca85e5b8ef2124bfdbacd084e6a
cconner57 <chris.conner57@gmail.com>
2019-10-15 14:35:54 -0700

    Added Refs

--- React/react-complete-guide/src/components/Cockpit/Cockpit.js
+++ React/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
-const cockpit = props => {
-    const assignedClasses = [];
-    let btnClass = '';
-    if (props.showPersons) {
-        btnClass = classes.Red;
-    }
-    if (props.persons.length <= 2) {
-      assignedClasses.push( classes.red );
-    }
-    if (props.persons.length <= 1) {
-      assignedClasses.push( classes.bold );
-    }
+const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    toggleBtnRef.current.click();
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); 
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); 
+  }
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
-      <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
 
-export default cockpit;
+export default React.memo(Cockpit);
