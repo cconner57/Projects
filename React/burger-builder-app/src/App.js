034480ffce4139400d4d7d5b962d756d4fa9e57a
cconner57 <chris.conner57@gmail.com>
2019-10-16 18:57:02 -0700

    Added Nav and Layout

--- React/burger-builder-app/src/App.js
+++ React/burger-builder-app/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render(){
+  render () {
     return (
-    <div>
-      <Layout>
-        <BurgerBuilder></BurgerBuilder>
-      </Layout>
-    </div>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     );
   }
 }
