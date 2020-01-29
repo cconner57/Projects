4989f37e1f3ec2c89a4c6c2127c1f016088eea05
Chris <chris.conner57@gmail.com>
2020-01-29 13:28:59 -0800

    Changed images

--- JavaScript/image-carousel/index.js
+++ JavaScript/image-carousel/index.js
@@ -1,9 +1,9 @@
 let i = 0;
 let images = [];
 
-images[0] = 'image1.jpg';
-images[1] = 'image2.jpg';
-images[2] = 'image3.jpg';
+images[0] = 'https://placeimg.com/640/480/animals';
+images[1] = 'https://placeimg.com/640/480/tech';
+images[2] = 'https://placeimg.com/640/480/nature';
 
 function changeImg() {
 	document.slide.src = images[i];
