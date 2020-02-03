43fa0617111135d2c0182520ad7ce7864993883b
Chris <chris.conner57@gmail.com>
2020-02-02 21:49:35 -0800

    First init

---
+++ React/jeopardy-app/src/Board.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+function Board() {
+	return (
+		<div className="container">
+			<div className="title">
+				<h1>Jeopardy</h1>
+			</div>
+			<div className="board">
+				<div className="categories">
+					<h5>Category 1</h5>
+				</div>
+				<div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+				<div className="categories">
+					<h5>Category 2</h5>
+				</div>
+				<div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+				<div className="categories">
+					<h5>Category 3</h5>
+				</div>
+                <div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+				<div className="categories">
+					<h5>Category 4</h5>
+				</div>
+                <div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+				<div className="categories">
+					<h5>Category 5</h5>
+				</div>
+                <div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+				<div className="categories">
+					<h5>Category 6</h5>
+				</div>
+                <div className="block">
+					<h5>$100</h5>
+				</div>
+				<div className="block">
+					<h5>$200</h5>
+				</div>
+				<div className="block">
+					<h5>$300</h5>
+				</div>
+				<div className="block">
+					<h5>$400</h5>
+				</div>
+				<div className="block">
+					<h5>$500</h5>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Board;
