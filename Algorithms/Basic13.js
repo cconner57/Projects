cf3aed6aeea444ef86b2c31521c7473bd2f2d3da
Chris <chris.conner57@gmail.com>
2020-02-15 21:54:06 -0800

    Practicing algorithms

---
+++ Algorithms/Basic13.js
@@ -0,0 +1,59 @@
+// 1. Print 1-255
+// Print1to255()
+// Print all the integers from 1 to 255
+
+for (i = 1; i < 256; i++) {
+	console.log(i);
+}
+
+// 2.Print Odds 1-255
+// PrintOdds1To255()
+// Print all odd integers from 1 to 255
+
+for (i = 1; i < 256; i += 2) {
+	console.log(i);
+}
+
+// 3.Print Ints and Sum 0-255
+// PrintIntsAndSum0To255()
+// Print integers from 0 to 255, and with each integer print the sum so far
+
+// 4.Interate and Print Array
+// PrintArrayVals(arr)
+// Iterate through a given Array, printing each value
+
+// 5.Find and Print Max
+// PrintMaxOfArray(arr)
+// Given an Array, find and print its largest element
+
+// 6.Get and Print Average
+// PrintAverageOfArray(arr)
+// Analyze an array's values and print the average
+
+// 7.Array with Odds
+// ReturnOddsArray1To255()
+// Create an array with all the odd integers between 1 and 255 (inclusive)
+
+// 8.Square the Values
+// SquareArrayVals(arr)
+// Square each value in a given Array, returning that same array with changed values
+
+// 9.Greater than Y
+// ReturnArrayCountGreaterThanY(arr, y)
+// Given an array and a value Y, count and print the number of array values greater than Y
+
+// 10.Zero Out Negative Numbers
+// ZeroOutArrayNegativeVals(arr)
+// Return the given Array, after setting any negative values to zero
+
+// 11.Max, Min, Average
+// PrintMaxMinAverageArrayVals(arr)
+// Given an Array, print the max, min, and average values for that array
+
+// 12.Shift Array Values
+// ShiftArrayValsLeft(arr)
+// Given an Array, move all values forward (to the left) by one indexedDB, dropping the first value and leaving a 0 (zero) value at the end of the array
+
+// 13.Swap String For Array Negative Values
+// SwapStringForArrayNegativeVals(arr)
+// Given an array of SVGAnimatedNumberList, replace any negative values with the string 'Dojo'
